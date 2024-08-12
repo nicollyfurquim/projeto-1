abadf8fe3fe75ccd381e8d935fab3e8ba4eb2cee
nicollyfurquim <00001091003531sp@al.educacao.sp.gov.br>
2024-08-12 17:10:44 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(1000,800);
+  background("rgb(203,102,204)");
+}
+
+function draw() {
+  fill("rgb(104,28,112)");
+  stroke("#EEE7F0,194)");
+  if (mouseIsPressed)
+  circle(mouseX,mouseY,50);
+}
